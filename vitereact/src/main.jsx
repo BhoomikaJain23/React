dad27ae9042ab7e589c9b489fc209694385194e1
BhoomikaJain23 <jainbhoomika024@gmail.com>
2026-01-15 02:09:15 +0530

    another way of custom react added

--- vitereact/src/main.jsx
+++ vitereact/src/main.jsx
@@ -1,10 +1,24 @@
-import { StrictMode } from 'react'
+import React, { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+const anotherELement=(
+  <a href="https://google.com" target='_blank'>Visit Google</a>
+)
+
+const anotherUser =" react";
+
+const ReactElement =React.createElement(
+  'a',
+  {href: 'https://google.com', target: '_blank'},
+  'click to visit google',
+  anotherUser
+)
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    {ReactElement}
     <App />
   </StrictMode>,
 )
