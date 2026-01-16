be1edc6bbf8405179c90ddd94c68ef1d358d1230
BhoomikaJain23 <jainbhoomika024@gmail.com>
2026-01-16 18:12:46 +0530

    add props to Card

--- tailwindprops/src/App.jsx
+++ tailwindprops/src/App.jsx
@@ -1,6 +1,5 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import Card from './Component/Card.jsx'
 import './App.css'
 
 function App() {
@@ -8,26 +7,12 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className='flex flex-col items-center justify-center min-h-screen bg-blue-300 text-white w-100vw'>
+      <h1 className='text-3xl bg-cyan-800 w-100 h-10 rounded-2xl mb-2 align-center justify-center'>Tailwind Test</h1>
+      <Card username="Life" />
+      <br />
+      <Card btnTxt='Learn More'/>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
